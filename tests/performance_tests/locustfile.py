d320ad64583f979ca7b70b2c39e257e5bdac48bc
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-06-23 17:15:24 +0200

    added performance_test with locust

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -5,6 +5,7 @@ from locust import HttpUser, between, task
 
 class MyUser(HttpUser):
     """A simple Locust user class that defines the tasks to be performed by the users."""
+
     host = "http://localhost:8000"
     wait_time = between(1, 2)
 
@@ -18,4 +19,3 @@ class MyUser(HttpUser):
         with open("tests/data/sample_image.PNG", "rb") as f:
             files = {"image_file": f}
             self.client.post("/api/predict", files=files)
-    
