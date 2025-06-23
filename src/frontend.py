d050f1d8145662af49493c9adf1f20395e9a277e
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-06-23 15:00:40 +0200

    added an image handler

---
+++ src/frontend.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image
+import io
+
+def image_upload():
+    uploaded_file = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+    
+    #TODO: Add function to predict the image & display the image with the prediction only
+
+
+def main():
+    st.title('Hello, world!')
+    image_upload()
+
+if __name__ == "__main__":
+    main()
