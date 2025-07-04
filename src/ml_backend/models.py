b54b87c78df616aac212be1868940a3d59e57e74
eunai9 <eunai9@naver.com>
2025-07-04 16:11:18 +0200

    model improvement

--- src/ml_backend/models.py
+++ src/ml_backend/models.py
@@ -1,43 +1,28 @@
+import os
 from typing import Any, Dict
 from ultralytics import YOLO
-import pickle
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 
-def train_model(model_name: str, epoch: int = 10) -> Any:
+def train_model(model_name: str = "simple", batch_size: int = -1, epochs: int = 10, wandb_logging: bool = False) -> Any:
     """Trains the machine learning model."""
-    pretrained_weights_path_dict = {"yolov8n": "models/yolov8n.pt", "yolov9n": "models/yolov9n.pt", "yolov11n": "models/yolov11n.pt",
-                                    "simple": "models/yolov8n.pt"}
-    yaml_file_path_dict = {"yolov8n": "data/BrainTumor/BrainTumorYolov8/data.yaml", "yolov9n": "data/BrainTumor/BrainTumorYolov9/data.yaml", "yolov11n": "data/BrainTumor/BrainTumorYolov11/data.yaml",
-                           "simple": "data/BrainTumor/BrainTumorSimple/data.yaml"}
-    print("Training model with pretrained weights:", pretrained_weights_path_dict[model_name])
-    # Placeholder for actual model training logic
-    T_Model = YOLO(pretrained_weights_path_dict[model_name])
-    results = T_Model.train(data= yaml_file_path_dict[model_name], epochs=epoch, patience=20, batch= -1, optimizer='auto',
+    # pretrained_weights_path_dict = {"yolov8n": "models/yolov8n.pt", "yolov9n": "models/yolov9n.pt", "yolov11n": "models/yolov11n.pt",
+    #                                 "simple": "models/simple.pt"}
+    yaml_data_path_dict = {"yolov8n": "data/BrainTumor/BrainTumorYolov8/data.yaml", "yolov9n": "data/BrainTumor/BrainTumorYolov9/data.yaml", "yolov11n": "data/BrainTumor/BrainTumorYolov11/data.yaml",
+                           "simple": "configs/data/data.yaml"}
+    
+    print("Training model with pretrained weights:", f"models/{model_name}.pt")
+    T_Model = YOLO(f"models/{model_name}.pt")
+    
+    if wandb_logging == True:
+        print("Initializing Weights & Biases for logging...")
+        wandb.login()
+        os.system("yolo settings wandb=True")
+        wandb.init(project="BrainTumorDetection", job_type="training",
+                #    config={"model_name": model_name, "batch_size": batch_size, "epochs": epochs},
+                   )
+        add_wandb_callback(T_Model)
+    
+    results = T_Model.train(data= yaml_data_path_dict[model_name], epochs=epochs, patience=20, batch= batch_size, optimizer='auto',
                             project="models/", name=model_name)
     return results
-
-def save_model(model: Any, model_path: str):
-    """Saves the trained model to a specified path."""
-    print(f"Saving model to: {model_path}")
-    # Placeholder for actual model saving logic
-    pass
-
-def load_model(model_path: str) -> Any:
-    """Loads a trained machine learning model."""
-    print(f"Loading model from: {model_path}")
-    # Placeholder for actual model loading logic
-    return "Mock Loaded Model"
-
-def predict(model: Any, X: Any) -> Dict:
-    """Makes predictions using the loaded model."""
-    print("Making predictions...")
-    # Placeholder for actual prediction logic
-    return {
-        "heartDiseaseRisk": 75,
-        "diabetesRisk": 60,
-        "confidence": 90,
-        "recommendations": [
-            "Consult a cardiologist",
-            "Monitor blood sugar levels",
-            "Adopt a healthy diet"
-        ]
-    }
--- src/ml_backend/train.py
+++ src/ml_backend/train.py
@@ -1,40 +1,34 @@
-from models import train_model, save_model
-from data import load_raw_data, clean_data, split_data
-from features import engineer_features, select_features
+import os
+from models import train_model
 from typing import Annotated
 import typer
+import hydra
 
 app = typer.Typer()
 
 @app.command()
-def run_training_pipeline(model_name: Annotated[str, typer.Option("--model_name", "-m")] = "yolov8n",
-                          epoch: Annotated[int, typer.Option("--epoch")] = 10,
+def run_training_typer(model_name: Annotated[str, typer.Option("--model_name", "-m")] = "simple",
+                          batch_size: Annotated[int, typer.Option("--batch_size", "-b")] = -1,
+                          epochs: Annotated[int, typer.Option("--epochs")] = 10,
+                          wandb_logging: Annotated[bool, typer.Option("--wandb", "-w")] = False,
                           ):
     print("Starting training pipeline...")
-
-    # 1. Load Data
-    # raw_data = load_raw_data(data_path)
-    # cleaned_data = clean_data(raw_data)
-
-    # 2. Feature Engineering
-    # features = engineer_features(cleaned_data)
-    # X = select_features(features) # Assuming select_features returns X_train for simplicity
-    # y = features['Outcome'] # Placeholder for target data
-
-    ## 2-1. Split Data
-    # X_train , X_test , y_train , y_test = split_data(X, y, test_size=0.3, random_state=random_state)
-
-    # 3. Model Training
-    # model_params = {"learning_rate": 0.01, "n_estimators": 100}
-    trained_model = train_model(model_name, epoch)
-
-    # 4. Save Model
-    # save_model(trained_model, model_output_path)
-
+    
+    train_model(model_name, batch_size, epochs, wandb_logging)
+    
     print("Training pipeline completed.")
 
+
+@hydra.main(config_name="config.yaml", config_path=f"configs/model")
+def run_training_hydra(cfg):
+    print("Starting training pipeline...")
+    
+    os.chdir(hydra.utils.get_original_cwd())
+    train_model(cfg.hyperparameters.model_name, cfg.hyperparameters.batch_size, 
+                cfg.hyperparameters.epochs, cfg.hyperparameters.wandb_logging)
+    
+    print("Training pipeline completed.")
+
+
 if __name__ == "__main__":
-    # MODEL_NAME = "yolov8n"
-    # EPOCH = 5
-    # run_training_pipeline(MODEL_NAME, EPOCH)
-    app()
+    run_training_hydra()
