60e486164b8a416db58ff228994339afb137e4e9
eunai9 <eunai9@naver.com>
2025-07-11 15:41:10 +0200

    edit a models.py to run configuration file well

--- ml/models.py
+++ ml/models.py
@@ -11,15 +11,7 @@ from wandb.integration.ultralytics import add_wandb_callback
 
 def train_model(model_name: str = "simple", batch_size: int = -1, epochs: int = 10, wandb_logging: bool = False) -> Any:
     """Trains the machine learning model."""
-    # pretrained_weights_path_dict = {"yolov8n": "ml/models/yolov8n.pt", "yolov9n": "ml/models/yolov9n.pt", "yolov11n": "ml/models/yolov11n.pt",
-    #                                 "simple": "ml/models/simple.pt"}
-    yaml_data_path_dict = {
-        "yolov8n": "data/BrainTumor/BrainTumorYolov8/data.yaml",
-        "yolov9n": "data/BrainTumor/BrainTumorYolov9/data.yaml",
-        "yolov11n": "data/BrainTumor/BrainTumorYolov11/data.yaml",
-        "simple": "configs/data/data.yaml",
-    }
-
+    
     print("Training model with pretrained weights:", f"ml/models/{model_name}.pt")
     T_Model = YOLO(f"ml/models/{model_name}.pt")
 
@@ -35,7 +27,7 @@ def train_model(model_name: str = "simple", batch_size: int = -1, epochs: int = 
         add_wandb_callback(T_Model)
 
     results = T_Model.train(
-        data=yaml_data_path_dict[model_name],
+        data="ml/configs/data/data.yaml",
         epochs=epochs,
         patience=20,
         batch=batch_size,
