69ba6c32d5291602f3ddb69a4b0a6709773f77b4
eunai9 <eunai9@naver.com>
2025-07-13 08:41:11 +0200

    edit pyproject.toml and tasks.py

---
+++ tasks.py
@@ -0,0 +1,26 @@
+from invoke import task
+import os
+
+@task
+def python(ctx):
+    """ """
+    ctx.run("which python" if os.name != "nt" else "where python")
+
+@task
+def git(ctx, message):
+    ctx.run(f"git add .")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
+
+@task
+def conda(ctx, name: str = "health-care"):
+    ctx.run(f"conda env create -f environment.yml", echo=True)
+    
+    ctx.run(f"source ~/.bashrc", echo=True)
+    ctx.run(f"conda init", echo=True)
+    ctx.run(f"conda activate {name}", echo=True)
+    ctx.run(f"pip install -e .", echo=True)
+
+@task
+def train(ctx):
+    ctx.run("train")
