2ff22f7b9620a860821d17b555ba85a0c9778ba7
eunai9 <eunai9@naver.com>
2025-07-13 09:41:05 +0200

    edit tasks.py

--- tasks.py
+++ tasks.py
@@ -2,11 +2,6 @@ from invoke import task
 import os
 
 @task
-def python(ctx):
-    """ """
-    ctx.run("which python" if os.name != "nt" else "where python")
-
-@task
 def git(ctx, message):
     ctx.run(f"git add .")
     ctx.run(f"git commit -m '{message}'")
